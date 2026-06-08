dd70209600f37a0c0147752ce7830f3e614e13b0
Nick <nicholas.currier@gmail.com>
2026-06-08 12:40:12 -0700

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker run -dit --network host --mount type=bind,source=C:/Users/nicho/Documents/docker_home_nick,target=/home/nick/ my_docker /bin/bash
+
+USER_NAME="nick"
+LOCAL_PATH="C:/Users/nicho/Documents/docker_home_$USER_NAME"
+IMAGE="docfreezzzz/my_docker"
+VERSION="v1.0"
+docker pull $IMAGE:$VERSION
+# Run docker headleass and mount the local path onto the home directory
+docker run -dit --network host --mount type=bind,source=$LOCAL_PATH,target=/home/$USER_NAME/ \
+  $IMAGE:$VERSION /bin/bash
+
+# To attach to this container run:
+# docker ps 
+# To get the name of a running session, thus:
+# docker attach <Container Name>
