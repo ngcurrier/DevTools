3796fa997cffc95e912308f2bb8feacf7072d9ac
Nick <nicholas.currier@gmail.com>
2026-06-08 13:38:51 -0700

    Bump version

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 USER_NAME="nick"
 LOCAL_PATH="C:/Users/nicho/Documents/docker_home_$USER_NAME"
 IMAGE="docfreezzzz/my_docker"
-VERSION="v1.0"
+VERSION="v1.1"
 docker pull $IMAGE:$VERSION
 # Run docker headleass and mount the local path onto the home directory
 docker run -dit --network host --mount type=bind,source=$LOCAL_PATH,target=/home/$USER_NAME/ \
