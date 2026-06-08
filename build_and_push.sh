7b862666cf8e721e732b323399a31670999363f4
Nick <nicholas.currier@gmail.com>
2026-06-08 12:48:16 -0700

    Create build_and_push.sh

---
+++ build_and_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# check number of args
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <Version Tag>" >&2
+    exit 1
+fi
+VERSION=$1
+IMAGE="my_docker"
+IMAGE_LOCATION="docfreezzzz"
+
+docker build -t $IMAGE:$VERSION
+docker push $IMAGE_LOCATION/$IMAGE:$VERSION
+echo "Built and pushed image to: $IMAGE_LOCATION/$IMAGE:$VERSION"
